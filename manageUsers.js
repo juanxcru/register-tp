3e958b94aacfa4f2881b1fe8ebe7326b6f24d2c3
alejocalvi <alejo.calvi@mediamonks.com>
2023-11-30 23:17:48 -0300

    ft: crear usuarios

--- manageUsers.js
+++ manageUsers.js
@@ -2,21 +2,78 @@ const begin = async () => {
     let usersData = await read();
     reloadTable2(usersData);
     document.getElementById('table-reg').addEventListener('click',deleditReg)
+    document.getElementById('crearReg').addEventListener('click',crearReg)
 };
 
 
 const deleditReg = async (event) => {
 
     const targetBtn = event.target;
-    if (targetBtn.classList.contains('edit-btn')) {
+    if (targetBtn.classList.contains('crear-btn')) {
       const regId = targetBtn.dataset.id;
-    //   editReg(regId)
+    //   crearReg(regId)
     } else if (targetBtn.classList.contains('delete-btn')) {
       const regId = targetBtn.dataset.id;
       deleteReg(regId)
     }
      
   };
+
+const saveUser = async () => {
+    console.log('SAVED')
+
+    let inputIdValue = document.getElementById('input-id').value
+    let inputNameValue = document.getElementById('input-name').value
+    let inputLastNameValue = document.getElementById('input-lastname').value
+    let inputEmailValue = document.getElementById('input-mail').value
+
+    let obj = {
+        id: inputIdValue,
+        name : inputNameValue,
+        lastname : inputLastNameValue,
+        email : inputEmailValue,
+        password : 'password',
+    }
+
+        let res = await fetch("http://localhost/TP-LAB-PROG/register-tp/services/registrarse.php",{
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify(obj)
+      });
+
+      let resjson = await res.json();
+
+      if(resjson.exito){
+        location.assign("http://localhost/TP-LAB-PROG/register-tp/login.html");
+      }else{
+        //invalids feedback
+        console.log(resjson.mensaje)
+      }
+}
+
+const crearReg = (id) => {
+    const table = document.querySelector("table");
+    const tbody = table.querySelector("tbody");
+
+
+        let row = document.createElement("tr");
+        row.innerHTML = `
+            <th><input id="input-id"/></th>
+            <td><input id="input-name"/></td>
+            <td><input id="input-lastname"/></td>
+            <td><input id="input-mail"/></td>
+            <td>
+                
+                <button class="btn btn-danger text-white aceptar-btn" id="btn-aceptar">Aceptar</button>
+            </td>`;
+
+        tbody.append(row);
+
+    document.getElementById('btn-aceptar').addEventListener('click', saveUser);
+
+}
 
 const deleteReg = async (id) => {
   
